7504f48a22dccb80dad2577cd12753c3a05426e2
Allen Bargi <allen.bargi@gmail.com>
2016-10-17 16:35:06 +0200

    fixed not being able to file global root

--- notes.py
+++ notes.py
@@ -174,6 +174,7 @@ class NotesNewCommand(sublime_plugin.ApplicationCommand):
 class NotesEvents(sublime_plugin.EventListener):
 
     def on_load_async(self, view):
+        global root
         if view.settings().get("is_note") or not view.file_name():
             return
         if os.path.realpath(view.file_name()).startswith(root):
